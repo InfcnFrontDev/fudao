d9bd4314cdc105bfea77ecae0cb1efb582206779
杨可可 <yangkk@infcn.com.cn>
2017-02-27 17:22:27 +0800

    modify router

--- app/AppNavigator.js
+++ app/AppNavigator.js
@@ -1,15 +1,20 @@
 import React, {Component} from "react";
 import {BackAndroid, StatusBar, NavigationExperimental, Platform, ToastAndroid} from "react-native";
 import {StyleProvider, Drawer} from "native-base";
-import SplashScreen from 'react-native-splash-screen'
+import SplashScreen from "react-native-splash-screen";
 import {connect} from "react-redux";
-import {Router, Scene, Reducer, Actions} from "react-native-router-flux";
+import {Router, Scene, Reducer} from "react-native-router-flux";
 import {openDrawer, closeDrawer} from "./actions/drawer";
 import getTheme from "../native-base-theme/components/";
 import SideBar from "./views/sidebar/";
-import {theme} from "./utils/"
+import {theme} from "./utils/";
 //
 import Index from "./views/index/";
+// tabs
+import Home from "./views/home/";
+import Article from "./views/article/";
+import Dynamic from "./views/dynamic/";
+import My from "./views/my/";
 import About from "./views/about/";
 import Protocol from "./views/protocol/";
 import Declare from "./views/declare/";
@@ -35,114 +40,122 @@ import Webview from "./views/webview/";
 
 const AppRouter = connect()(Router);
 const {
-	CardStack: NavigationCardStack,
+    CardStack: NavigationCardStack,
 } = NavigationExperimental;
 
 var lastBackPressTime = 0;
 class AppNavigator extends Component {
 
-	componentDidUpdate() {
-		if (this.props.drawerState === 'opened') {
-			this._drawer._root.open();
-		}
+    componentDidUpdate() {
+        if (this.props.drawerState === 'opened') {
+            this._drawer._root.open();
+        }
 
-		if (this.props.drawerState === 'closed') {
-			this._drawer._root.close();
-		}
-	}
+        if (this.props.drawerState === 'closed') {
+            this._drawer._root.close();
+        }
+    }
 
-	openDrawer() {
-		this._drawer._root.open();
-	}
+    openDrawer() {
+        this._drawer._root.open();
+    }
 
-	closeDrawer() {
-		if (this.props.drawerState === 'opened') {
-			this.props.closeDrawer();
-		}
-	}
+    closeDrawer() {
+        if (this.props.drawerState === 'opened') {
+            this.props.closeDrawer();
+        }
+    }
 
-	render() {
-		return (
-			<StyleProvider style={getTheme(theme)}>
-				<Drawer
-					ref={(ref) => {
+    render() {
+        return (
+            <StyleProvider style={getTheme(theme)}>
+                <Drawer
+                    ref={(ref) => {
 						this._drawer = ref;
 					}}
-					content={<SideBar navigator={this._navigator}/>}
-					onClose={() => this.closeDrawer()}>
-					<StatusBar
-						hidden={(this.props.drawerState === 'opened' && Platform.OS === 'ios') ? true : false}
-						backgroundColor={theme.statusBarColor}/>
-					<AppRouter
-						createReducer={this.reducerCreate.bind(this)}
-						onExitApp={this.appExit.bind(this)}>
-						<Scene key="root">
-							<Scene key="index" component={Index} hideNavBar tabs initial={true} title="首页"/>
-							<Scene key="about" component={About} title="关于福道"/>
-							<Scene key="protocol" component={Protocol} title="用户协议"/>
-							<Scene key="declare" component={Declare} title="隐式声明"/>
+                    content={<SideBar navigator={this._navigator}/>}
+                    onClose={() => this.closeDrawer()}>
+                    <StatusBar
+                        hidden={(this.props.drawerState === 'opened' && Platform.OS === 'ios') ? true : false}
+                        backgroundColor={theme.statusBarColor}/>
+                    <AppRouter
+                        createReducer={this.reducerCreate.bind(this)}
+                        onExitApp={this.appExit.bind(this)}>
+                        <Scene key="root">
 
-							<Scene key="search" component={Search} title="搜索"/>
-							<Scene key="searchDailyLife" component={SearchDailyLife} title="日常生活"/>
-							<Scene key="searchFriendsCircle" component={SearchFriendsCircle} title="朋友圈"/>
-							<Scene key="searchHealthCare" component={SearchHealthCare} title="保健方法"/>
-							<Scene key="searchInformation" component={SearchInformation} title="资讯"/>
-							<Scene key="searchOfflineService" component={SearchOfflineService} title="隐式声明"/>
-							<Scene key="searchSymptomProblem" component={SearchSymptomProblem} title="隐式声明"/>
+                            <Scene key="tabbar" duration={0} tabs initial>
+                                <Scene key="home" duration={0} component={Home} title="主页"/>
+                                <Scene key="article" duration={0} component={Article} title="资讯"/>
+                                <Scene key="dynamic" duration={0} component={Dynamic} title="动态"/>
+                                <Scene key="my" duration={0} component={My} title="我的"/>
+                            </Scene>
 
-							<Scene key="articleDetail" component={ArticleDetail} title="资讯详情"/>
-							<Scene key="picture" component={Picture} title="图片预览"/>
-							{/*启动后开始页*/}
-							<Scene key="start" component={Start}/>
-							{/*登录页*/}
-							<Scene key="login" component={Login}/>
-							{/*注册页*/}
-							<Scene key="register" component={Register}/>
-							{/*设置密码*/}
-							<Scene key="setPassword" component={SetPassword}/>
-							{/*注册成功*/}
-							<Scene key="passwordSuccess" component={PasswordSuccess}/>
-							{/*首次登录添个人信息*/}
-							<Scene key="startInformation" component={StartInformation}/>
-							<Scene key="myInfo" component={MyInfo} title="个人信息"/>
-							<Scene key="webview" component={Webview} title="WebView"/>
-						</Scene>
-					</AppRouter>
-				</Drawer>
-			</StyleProvider>
-		)
-	}
+                            <Scene key="index" component={Index} hideNavBar tabs initial={true} title="首页"/>
+                            <Scene key="about" component={About} title="关于福道"/>
+                            <Scene key="protocol" component={Protocol} title="用户协议"/>
+                            <Scene key="declare" component={Declare} title="隐式声明"/>
 
-	componentDidMount() {
-		SplashScreen.hide();
-	}
+                            <Scene key="search" component={Search} title="搜索"/>
+                            <Scene key="searchDailyLife" component={SearchDailyLife} title="日常生活"/>
+                            <Scene key="searchFriendsCircle" component={SearchFriendsCircle} title="朋友圈"/>
+                            <Scene key="searchHealthCare" component={SearchHealthCare} title="保健方法"/>
+                            <Scene key="searchInformation" component={SearchInformation} title="资讯"/>
+                            <Scene key="searchOfflineService" component={SearchOfflineService} title="隐式声明"/>
+                            <Scene key="searchSymptomProblem" component={SearchSymptomProblem} title="隐式声明"/>
 
-	reducerCreate(params) {
-		const defaultReducer = Reducer(params)
-		return (state, action) => {
-			// console.log(state);
-			// console.log(action);
-			return defaultReducer(state, action)
-		}
-	}
+                            <Scene key="articleDetail" component={ArticleDetail} title="资讯详情"/>
+                            <Scene key="picture" component={Picture} title="图片预览"/>
+                            {/*启动后开始页*/}
+                            <Scene key="start" component={Start}/>
+                            {/*登录页*/}
+                            <Scene key="login" component={Login}/>
+                            {/*注册页*/}
+                            <Scene key="register" component={Register}/>
+                            {/*设置密码*/}
+                            <Scene key="setPassword" component={SetPassword}/>
+                            {/*注册成功*/}
+                            <Scene key="passwordSuccess" component={PasswordSuccess}/>
+                            {/*首次登录添个人信息*/}
+                            <Scene key="startInformation" component={StartInformation}/>
+                            <Scene key="myInfo" component={MyInfo} title="个人信息"/>
+                            <Scene key="webview" component={Webview} title="WebView"/>
+                        </Scene>
+                    </AppRouter>
+                </Drawer>
+            </StyleProvider>
+        )
+    }
 
-	appExit() {
-		if (lastBackPressTime && lastBackPressTime + 2000 >= Date.now()) {
-			return false;
-		}
-		lastBackPressTime = Date.now();
-		ToastAndroid.show('再按一次退出应用', ToastAndroid.SHORT);
-		return true;
-	}
+    componentDidMount() {
+        SplashScreen.hide();
+    }
+
+    reducerCreate(params) {
+        const defaultReducer = Reducer(params)
+        return (state, action) => {
+            // console.log(state);
+            // console.log(action);
+            return defaultReducer(state, action)
+        }
+    }
+
+    appExit() {
+        if (lastBackPressTime && lastBackPressTime + 2000 >= Date.now()) {
+            return false;
+        }
+        lastBackPressTime = Date.now();
+        ToastAndroid.show('再按一次退出应用', ToastAndroid.SHORT);
+        return true;
+    }
 }
 
 const bindAction = dispatch => ({
-	openDrawer: () => dispatch(openDrawer()),
-	closeDrawer: () => dispatch(closeDrawer()),
+    openDrawer: () => dispatch(openDrawer()),
+    closeDrawer: () => dispatch(closeDrawer()),
 });
 
 const mapStateToProps = state => ({
-	drawerState: state.drawer.drawerState,
+    drawerState: state.drawer.drawerState,
 });
 
 export default connect(mapStateToProps, bindAction)(AppNavigator);
