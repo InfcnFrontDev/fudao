63f9e97f9ce1f216d72464b22ebb29d8aa2bb08e
杨可可 <yangkk@infcn.com.cn>
2017-03-03 18:59:53 +0800

    Merge branch 'dev' of http://192.168.10.158/InfcnFrontDev/fudao into dev
    # Conflicts:
    #	app/AppNavigator.js
    #	app/utils/urls.js

--- app/utils/urls.js
+++ app/utils/urls.js
@@ -1,10 +1,10 @@
 // 接口服务器地址
- // const apiPath = 'http://103.254.113.11:8080/jkst2/'; // 外网
+// const apiPath = 'http://103.254.113.11:8080/jkst2/'; // 外网
 // const apiPath = 'http://192.168.10.69:8080/jkst2/'; // 内网服务器
- const apiPath = 'http://192.168.3.126:18080/fudao-svc/'; // 谢鹏
+const apiPath = 'http://192.168.3.126:18080/fudao-svc/'; // 谢鹏
 // const apiPath = 'http://192.168.3.241:8080/center/ifcuas/'; // txh
 //  const apiPath = 'http://192.168.3.241:8080/center/ifcuas/'; // txh
-http://192.168.10.69:8080/center/ajaxtest.jsp
+// http://192.168.10.69:8080/center/ajaxtest.jsp
 
 // web服务器地址
 const webPath = 'http://192.168.3.204:3000/';
@@ -35,13 +35,13 @@ const urls = {
 		// 我的时间
 		MY_TIME: webPath + 'myTime.html',
 		// 健康测评
-		HEALTH_APPRAISAL : webPath + 'healthAppraisal.html',
+		HEALTH_APPRAISAL: webPath + 'healthAppraisal.html',
 		// 测试题
-		ASSESSMENT_TEST : webPath + 'assessmentTest.html',
+		ASSESSMENT_TEST: webPath + 'assessmentTest.html',
 		// 健康圈
-		HEALTH_CIRCLE : webPath + 'healthCircle.html',
+		HEALTH_CIRCLE: webPath + 'healthCircle.html',
 		// 生命周期
-		LIFE_CYCLE : webPath + 'lifeCycle.html'
+		LIFE_CYCLE: webPath + 'lifeCycle.html'
 	},
 	/**
 	 *  接口
@@ -64,39 +64,30 @@ const urls = {
 		//验证验证码
 		CHECK_CODE: apiPath + "app/accountInfoAction!registerBefore.action",
 		REG: apiPath + "app/accountInfoAction!register.action",
-	}
-	 	LOGIN : apiPath + "app/accountInfoAction!login.action",
+		LOGIN: apiPath + "app/accountInfoAction!login.action",
 
 		// 注册登录(新)
 
 		/*//检测用户是否存在
-		CHECK_PHONE: apiPath + 'checkUserExists',
-		//发送验证码
-		SEND_CODE: apiPath + "sendMsg",
-		//检测验证码
-		CHECK_CODE: apiPath + "checkCodeIsOut",
-		//用户注册
-		REGISTER: apiPath + 'register',
-		//用户登录
-		LOGIN: apiPath + 'login',
-		//修改用户信息
-		SAVE_USERINFO: apiPath + 'saveUserInfo',
-		//重设密码
-		CHANGE_PASSWORD: apiPath + 'changePassword',
-		//检测旧密码
-		CHECK_PASSWORD: apiPath + 'checkPassword',
-		//获取用户信息
-		USER_INFO: apiPath + 'userinfo',*/
+		 CHECK_PHONE: apiPath + 'checkUserExists',
+		 //发送验证码
+		 SEND_CODE: apiPath + "sendMsg",
+		 //检测验证码
+		 CHECK_CODE: apiPath + "checkCodeIsOut",
+		 //用户注册
+		 REGISTER: apiPath + 'register',
+		 //用户登录
+		 LOGIN: apiPath + 'login',
+		 //修改用户信息
+		 SAVE_USERINFO: apiPath + 'saveUserInfo',
+		 //重设密码
+		 CHANGE_PASSWORD: apiPath + 'changePassword',
+		 //检测旧密码
+		 CHECK_PASSWORD: apiPath + 'checkPassword',
+		 //获取用户信息
+		 USER_INFO: apiPath + 'userinfo',*/
 
-	},
-
-	/**
-	 *  test
-	 */
-	test: 'http://wordnet.infcn.com.cn/user/login?username=admin&password=admin',
-	json: 'http://192.168.1.137:3000/data.json',
-	login: 'http://192.168.10.9:7071/qdeMods/login',
-
+	}
 
 };
 export default urls;
