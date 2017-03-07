e1051b768d62abe88a7b8ed36280a0298e5562af
kecoyo <kecoyo@163.com>
2017-03-07 23:47:29 +0800

    图片选择

--- app/views/my/components/MyPhoto.js
+++ app/views/my/components/MyPhoto.js
@@ -1,24 +1,55 @@
 import React, {PureComponent} from "react";
 import {TouchableOpacity, Image} from "react-native";
-import {Actions} from "react-native-router-flux";
 import {Left, Right, Body, Thumbnail, Item} from "native-base";
 import {theme} from "../../../utils/index";
+import ImagePicker from "react-native-image-picker"; //第三方相机
+var photoOptions = {
+	//底部弹出框选项
+	title: '请选择',
+	cancelButtonTitle: '取消',
+	takePhotoButtonTitle: '拍照',
+	chooseFromLibraryButtonTitle: '选择相册',
+	quality: 0.75,
+	allowsEditing: true,
+	noData: false,
+	storageOptions: {
+		skipBackup: true,
+		path: 'images'
+	}
+}
 
 /**
  * my grid menu
  */
 class MyPhoto extends PureComponent {
 
+	state = {
+		photo: require('../../../assets/my-photos/photo.jpg'),
+		path: null
+	}
+
 	render() {
+		let {photo, path} = this.state;
 		return (
 			<Thumbnail source={require('../../../assets/my-covers/pic01.jpg')} style={styles.myCover}>
-				<TouchableOpacity activeOpacity={1} onPress={()=> Actions.myInfo()}>
-					<Thumbnail
-						source={require('../../../assets/my-photos/photo.jpg')}
-						style={styles.myPhoto}/>
+				<TouchableOpacity activeOpacity={1} onPress={()=> this.cameraAction()}>
+					<Image style={styles.myPhoto} source={path ? {uri: path} : photo}/>
 				</TouchableOpacity>
 			</Thumbnail>
 		)
+	}
+
+	cameraAction = () => {
+		// {data, fileName, fileSize, height, isVertical, originalRotation, path, type, uri, width}
+		ImagePicker.showImagePicker(photoOptions, (response) => {
+			console.log(response);
+			if (response.didCancel) {
+				return
+			}
+			this.setState({
+				path: response.uri
+			});
+		})
 	}
 }
 
@@ -31,9 +62,9 @@ const styles = {
 		height: 180,
 	},
 	myPhoto: {
-		width: 80,
-		height: 80,
-		borderRadius: 40,
+		width: 100,
+		height: 100,
+		borderRadius: 50,
 	},
 };
 
