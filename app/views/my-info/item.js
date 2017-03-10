367cb683ba565bf67752749239f7e5e967349bae
wangp <wangp@infcn.com.cn>
2017-03-10 15:44:23 +0800

    个人信息

--- app/views/my-info/item.js
+++ app/views/my-info/item.js
@@ -1,7 +1,23 @@
 import React, {PureComponent} from "react";
-import {ToastAndroid} from "react-native";
+import {ToastAndroid,DatePickerAndroid} from "react-native";
 import {Left, Right, Body, ListItem, Text, Icon} from "native-base";
 import {dialogs} from "../../utils/";
+import ImagePicker from "react-native-image-picker";
+import chooseDate from "./chooseDate"
+var photoOptions = {
+    //底部弹出框选项
+    title: '请选择',
+    cancelButtonTitle: '取消',
+    takePhotoButtonTitle: '拍照',
+    chooseFromLibraryButtonTitle: '选择相册',
+    quality: 0.75,
+    allowsEditing: true,
+    noData: false,
+    storageOptions: {
+        skipBackup: true,
+        path: 'images'
+    }
+}
 
 
 /**
@@ -9,6 +25,34 @@ import {dialogs} from "../../utils/";
  * InputType.TYPE_CLASS_TEXT|InputType.TYPE_TEXT_VARIATION_PASSWORD = 0x81
  */
 class MyInfoItem extends PureComponent {
+    constructor(props) {
+        super(props);
+        this.state={
+            showM:true,
+            simpleText:"03/22/06",
+        }
+    }
+
+    async showPicker(stateKey, options) {
+        let option={
+            options,
+            mode:'spinner'
+        }
+        try {
+            var newState = {};
+            const {action, year, month, day} = await DatePickerAndroid.open(option);
+            if (action === DatePickerAndroid.dismissedAction) {
+                newState[stateKey + 'Text'] = 'dismissed';
+            } else {
+                var date = new Date(year, month, day);
+                newState[stateKey + 'Text'] = date.toLocaleDateString();
+                newState[stateKey + 'Date'] = date;
+            }
+            this.setState(newState);
+        } catch ({code, message}) {
+            console.warn(`Error in example '${stateKey}': `, message);
+        }
+    }
 
     render() {
         let {data} = this.props;
@@ -29,6 +73,9 @@ class MyInfoItem extends PureComponent {
         let {data} = this.props;
 
         switch (data.text) {
+            case '我的头像':
+                this.cameraAction();
+                break;
             case '姓名':
                 this.updateUsername(data);
                 break;
@@ -36,10 +83,13 @@ class MyInfoItem extends PureComponent {
                 this.updateSex(data);
                 break;
             case '出生日期':
-
+                this.showPicker();
                 break;
             case '身高':
-
+                this.updateStature(data);
+                break;
+            case '个人史':
+                this.multiChoiceDialog('个人史',chooseDate.PersonalHistory);
                 break;
         }
 
@@ -70,43 +120,44 @@ class MyInfoItem extends PureComponent {
             }
         });
     }
-
-    multiChoiceDialog() {
+    updateStature(data) {
         dialogs.showDialog({
-            title: "性别",
-            items: [
-                "男",
-                "女",
-                "男",
-                "女",
-                "男",
-                "女",
-                "男",
-                "女",
-                "男",
-                "女",
-                "男",
-                "女",
-                "男",
-                "女",
-                "男",
-                "女",
-                "男",
-                "女",
-                "男",
-                "女",
-                "男",
-                "女",
-                "男",
-                "女",
-                "男",
-                "女",
-            ],
-            selectedIndices: [0, 1],
+            title: data.text,
+            input: {
+                hint: 'User Name',
+                prefill: '',
+                allowEmptyInput: false,
+                maxLength: 10,
+                callback: (id, text) => ToastAndroid.show(id + ": " + text, ToastAndroid.SHORT),
+            }
+        });
+    }
+
+    cameraAction = () => {
+        // {data, fileName, fileSize, height, isVertical, originalRotation, path, type, uri, width}
+        ImagePicker.showImagePicker(photoOptions, (response) => {
+            console.log(response);
+            if (response.didCancel) {
+                return
+            }
+            this.setState({
+                path: response.uri
+            });
+        })
+    }
+
+
+
+    multiChoiceDialog(title,chooseDate) {
+        dialogs.showDialog({
+            title: title,
+            items:chooseDate,
+            selectedIndices: [],
             positiveText: "确定",
             itemsCallbackMultiChoice: (id, text) => ToastAndroid.show(id + ": " + text, ToastAndroid.SHORT),
         })
     }
+
 }
 
 MyInfoItem.propTypes = {
