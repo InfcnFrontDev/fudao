5a53ca1f22f3a52f38f7219fd8a5dd75796450d9
guoxm <guoxm@infcn.com.cn>
2017-03-14 13:58:58 +0800

    2

--- app/AppRouter.js
+++ app/AppRouter.js
@@ -58,13 +58,13 @@ class AppRouter extends PureComponent {
 			<RouterWithRedux createReducer={this.reducerCreate.bind(this)} onExitApp={this.appExit.bind(this)}>
 				<Scene key="root">
 
-					<Scene key="start" component={Start} title="启动开始页" hideNavBar  />
+					<Scene key="start" component={Start} title="启动开始页" hideNavBar initial />
 
 					{/*首页*/}
 					<Scene key="index" component={Index} title="首页" hideNavBar/>
 
 					{/*启动注册*/}
-					<Scene key="login" initial  component={Login} title="登录" hideNavBar/>
+					<Scene key="login"   component={Login} title="登录" hideNavBar/>
 					<Scene key="register"   component={Register} title="注册" hideNavBar/>
 					<Scene key="setPassword"  component={SetPassword} title="设置密码" hideNavBar/>
 					<Scene key="rebuildPassword" component={RebuildPassword} title="请设置新密码" hideNavBar/>
