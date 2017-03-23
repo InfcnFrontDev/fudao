433da8c2410e6ed16d484e005b3df754e7a0e947
杨可可 <yangkk@infcn.com.cn>
2017-03-23 12:38:31 +0800

    我的能量场

---
+++ app/views/energy/EnergyQuestionnaire.js
@@ -0,0 +1,27 @@
+import React, {PureComponent} from "react";
+import {View, Image, DeviceEventEmitter} from "react-native";
+import {connect} from "react-redux";
+import {Container, Header, Content, WebView} from "../../components/index";
+
+/**
+ * 我的能量场 > 问题调查
+ */
+class EnergyQuestionnaire extends PureComponent {
+
+	render() {
+		return (
+			<Container>
+				<Header {...this.props} />
+				<Content>
+					<WebView uri="http://echarts.baidu.com/examples.html"/>
+				</Content>
+			</Container>
+		)
+	}
+}
+
+
+const styles = {};
+
+const mapStateToProps = state => ({});
+export default connect(mapStateToProps)(EnergyQuestionnaire);
