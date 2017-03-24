7710179980b6ad9f0661682166c9700598f35ec3
杨可可 <yangkk@infcn.com.cn>
2017-03-24 14:07:47 +0800

    我的能量场

--- app/AppRouter.js
+++ app/AppRouter.js
@@ -142,8 +142,8 @@ class AppRouter extends PureComponent {
 					<Scene key="agreeFriendApply" component={AgreeFriendApply} title="好友验证" hideNavBar/>
 
 					{/*我的能量场*/}
-					<Scene key="myEnergy" component={MyEnergy} title="我的能量场" hideNavBar/>
-					<Scene key="energyInformation" component={EnergyInformation} title="资料填写" hideNavBar/>
+					<Scene key="myEnergy" component={MyEnergy} title="我的能量场" hideNavBar initial/>
+					<Scene key="energyInformation" component={EnergyInformation} title="资料填写" hideNavBar initial/>
 					<Scene key="energyQuestionnaire" component={EnergyQuestionnaire} title="问卷调查" hideNavBar/>
 
 					{/*侧边栏*/}
