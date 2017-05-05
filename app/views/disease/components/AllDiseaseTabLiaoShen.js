a399a77056299e4ddf38cb7c2a7166d7ea4c9c78
zhangyan <459685710@qq.com>
2017-05-05 18:06:02 +0800

    自疗

--- app/views/disease/components/AllDiseaseTabLiaoShen.js
+++ app/views/disease/components/AllDiseaseTabLiaoShen.js
@@ -1,5 +1,5 @@
 import React, {PureComponent} from "react";
-import {View, Image, ListView, TouchableHighlight} from "react-native";
+import {View, Image, ListView, TouchableHighlight,DeviceEventEmitter} from "react-native";
 import {Text, Button} from "native-base";
 
 export default class AllDiseaseTabLiaoShen extends PureComponent {
@@ -17,57 +17,131 @@ export default class AllDiseaseTabLiaoShen extends PureComponent {
 		onItemPress: () => console.log('onItemPress'),
 	}
 
-	dataSource = new ListView.DataSource({
-		rowHasChanged: (row1, row2) => row1 !== row2,
-		sectionHeaderHasChanged: (section1, section2) => section1 !== section2,
-	})
+    ds = new ListView.DataSource({
+        rowHasChanged: (r1, r2) => r1 !== r2,
+        sectionHeaderHasChanged: (section1, section2) => section1 !== section2,
+
+    });
+
+    _renderSectionHeader(sectionData, sectionID) {
+        return (
+            <View style={styles.sectionHeader}>
+                <Text style={{fontWeight: 'bold'}}>{sectionID}</Text>
+            </View>
+        )
+    }
+
+    _renderRow(rowData, sectionId, rowId) {
+        let {selectedItem, onItemAdd, onItemPress} = this.props;
+        let contains = false
+        selectedItem.map((item,index)=>{
+            if(item.id === rowData.id){
+                contains = true;
+            }
+        })
+        return (
+            <View style={styles.row}>
+                <Button transparent style={{padding: 0, margin: 6}} onPress={() => onItemPress(rowData)}>
+                    <View style={styles.rowView}>
+                        <Image source={{uri: urls.getImage(rowData.img)}} style={styles.rowimg}/>
+                        <Text style={styles.rowTitle}>{rowData.name}</Text>
+                        {contains ?
+                            <TouchableHighlight underlayColor='transparent'>
+                                <Image source={require('../../../assets/arrows_square_check.png')}
+                                       style={styles.choose}/>
+                            </TouchableHighlight>
+                            :
+							<TouchableHighlight onPress={() => onItemAdd(rowData)} underlayColor='transparent'>
+                                <Image source={require('../../../assets/arrows_square_plus.png')}
+                                       style={styles.choose}/>
+                            </TouchableHighlight>
+                        }
+                    </View>
+                </Button>
+            </View>
+        )
+    }
 
 	render() {
-		let {data} = this.props;
-		return (
+        let data = this.props.data
+        // let data = {
+        //     "意外伤害": [
+        //     {
+        //         "id": "077fbcc94bd843f7a308519bc4f9b532",
+        //         "img": "/icons/disease/dianxian.png",
+        //         "name": "癫痫",
+        //     },
+        // ],
+        //     "日常问题": [
+        //     {
+        //         "id": "131f725025e04c1485c9463bcf0e8b86",
+        //         "img": "/icons/disease/toutong.png",
+        //         "name": "头痛",
+        //     },
+        //     {
+        //         "id": "16b6ead6e3824c319fc67c5e4192c1b0",
+        //         "img": "/icons/disease/kouqiangkuiyang.png",
+        //         "name": "口腔溃疡",
+        //     },
+        //     {
+        //         "id": "1fd3a62662d24e6390b5e535b0bdf6c4",
+        //         "img": "/icons/disease/fuxie.png",
+        //         "name": "腹泻",
+        //     },
+        // ],
+        //     "衰老问题": [
+        //     {
+        //         "id": "17f7d8511be64daf985b883c1dddb1be",
+        //         "img": "/icons/disease/fanyingchidun.png",
+        //         "name": "反应迟钝",
+        //     },
+        //     {
+        //         "id": "19f67244a0a342c9b57b755c766423c1",
+        //         "img": "/icons/disease/feipang.png",
+        //         "name": "肥胖",
+        //     },
+        //     {
+        //         "id": "2312515984574208925f87820a72fc75",
+        //         "img": "/icons/disease/dabianshijin.png",
+        //         "name": "大便失禁",
+        //     },
+        //     {
+        //         "id": "245719653ed042ff9b41abfdfc26a666",
+        //         "img": "/icons/disease/niaoshijin.png",
+        //         "name": "尿失禁",
+        //     },
+        //     {
+        //         "id": "081f638682084b558d2b3ba08ae89dad",
+        //         "img": "/icons/disease/kouqiangganzao.png",
+        //         "name": "口腔干燥",
+        //     },
+        //     {
+        //         "id": "103e3f772cc042a78f93d33db53521c7",
+        //         "img": "/icons/disease/pifusaoyang.png",
+        //         "name": "皮肤瘙痒",
+        //     }
+        // ]
+        // }
+        if(data){
+            let liaoShen = {}
+            for(let k in data){
+                liaoShen[k] = data[k].slice()
+            }
+            data = liaoShen
+        }
+        return (
 			<ListView
-				contentContainerStyle={styles.contentContainer}
-				dataSource={this.dataSource.cloneWithRowsAndSections(data)}
+                enableEmptySections={true}
+                contentContainerStyle={styles.contentContainer}
+				dataSource={this.ds.cloneWithRowsAndSections(data)}
+				renderSectionHeader={this._renderSectionHeader.bind(this)}
 				renderRow={this._renderRow.bind(this)}
 				pageSize={4}
-				renderSectionHeader={this._renderSectionHeader.bind(this)}
+
 			/>
 		)
 	}
 
-	_renderSectionHeader(sectionData, sectionID) {
-		return (
-			<View style={styles.sectionHeader}>
-				<Text style={{fontWeight: 'bold'}}>{sectionID}</Text>
-			</View>
-		)
-	}
-
-	_renderRow(rowData, sectionId, rowId) {
-		let {selectedItem, onItemAdd, onItemPress} = this.props;
-		// alert(rowData.img)
-		return (
-			<View style={styles.row}>
-				<Button transparent style={{padding: 0, margin: 6}} onPress={() => onItemPress(rowData)}>
-					<View style={styles.rowView}>
-						<Image source={{uri: urls.getImage(rowData.img)}} style={styles.rowimg}/>
-						<Text style={styles.rowTitle}>{rowData.name}</Text>
-						{selectedItem[rowData.id] ?
-							<TouchableHighlight underlayColor='transparent'>
-								<Image source={require('../../../assets/arrows_square_check.png')}
-									   style={styles.choose}/>
-							</TouchableHighlight>
-							:
-							<TouchableHighlight onPress={() => onItemAdd(rowData)} underlayColor='transparent'>
-								<Image source={require('../../../assets/arrows_square_plus.png')}
-									   style={styles.choose}/>
-							</TouchableHighlight>
-						}
-					</View>
-				</Button>
-			</View>
-		)
-	}
 }
 
 const styles = {
