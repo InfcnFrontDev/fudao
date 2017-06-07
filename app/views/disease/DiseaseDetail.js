6708511c5964ee6f1db5fc20bcdebbde98e7dfa5
zhangyan <459685710@qq.com>
2017-06-07 14:51:06 +0800

    package.json

--- app/views/disease/DiseaseDetail.js
+++ app/views/disease/DiseaseDetail.js
@@ -62,9 +62,9 @@ export default class DiseaseDetail extends PureComponent {
 					{/*<Button transparent style={styles.btnStyle} onPress={()=> questionStore.jlModalShow =true}>*/}
 						{/*<Image source={require('../../assets/disease/jingluo.png')} style={styles.image}/>*/}
 					{/*</Button>*/}
-                    <Button transparent style={styles.btnStyle} onPress={() => Actions.body({pageKey:'disease',title:selectedItem.name})}>
+                    {allDiseaseListStore.selectedItem.type == '精神' ? <View/> : <Button transparent style={styles.btnStyle} onPress={() => Actions.body({pageKey:'disease',title:selectedItem.name})}>
                         <Image source={require('../../assets/disease/jingluo.png')} style={styles.image}/>
-                    </Button>
+                    </Button>}
 				</Content>
                 <DetailModal visible={modalShow}>
                     <QuestionText data={questionId} from={'disease'}/>
