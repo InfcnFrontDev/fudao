07090f0fe5f4dab2c698e0e767bc53510da86349
zhangyan <459685710@qq.com>
2017-06-07 10:49:15 +0800

    自疗

--- app/mobx/allExpectListStore.js
+++ app/mobx/allExpectListStore.js
@@ -25,7 +25,24 @@ class AllExpectListStore {
         });
 	}
     fetchData(){
-        request.getJson(urls.apis.DISEASE_ACUPOINTS,{diseaseType:this.selectedItem.type,disease: this.selectedItem.name})
+        let url
+        switch (type) {
+            case '1':
+                url = urls.apis.EXPECT_MERIDIANS;
+                break
+            case '2':
+                url = urls.apis.EXPECT_ACUPOINTS;
+                break
+            case '3':
+                url = urls.apis.EXPECT_ORGANS;
+                break
+            case '4':
+                url = urls.apis.EXPECT_CIRCULATORYSYSTEMS;
+                break
+            default:
+                return
+        }
+        request.getJson(url,{diseaseType:this.selectedItem.type,disease: this.selectedItem.name})
             .then((result) => {
                 if (result.ok) {
                     this.data = result.obj
