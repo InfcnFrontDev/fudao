fb41b11652d67232be6036c737675d5537787240
wangp <wangp@infcn.com.cn>
2017-06-09 18:38:42 +0800

    loc

--- app/views/energy/Energy.js
+++ app/views/energy/Energy.js
@@ -1,16 +1,38 @@
 import React, {PureComponent} from "react";
 import {View, Image, DeviceEventEmitter} from "react-native";
 import {Container, Header, Content, WebView} from "../../components/index";
+import userStore from "../../mobx/userStore";
+import weatherStore from "../../mobx/weatherStore";
 
 /**
  * 我的能量场
  */
 export default class Energy extends PureComponent {
+	componentWillMount(){
+		userStore.getposition();
+		let m='';
+
+		if(userStore.location.addressComponent){
+			m=userStore.location.addressComponent.city
+		}else{
+			m="北京市"
+		}
+		weatherStore.fetchCurrentWeather(m);
+
+	}
 
 	render() {
-		let city = "北京";
-		let weather = "晴";
-		let winp = "5级";
+		let {location} = userStore;
+		let {currentWeather} = weatherStore;
+		let m='';
+		if(location.addressComponent){
+			m=location.addressComponent.city+'.'+location.addressComponent.district
+		}else{
+			m='北京市';
+		}
+		let city = m;
+		let weather = currentWeather.weather;
+		let winp = currentWeather.winp;
 		let air_scope = "50-100";
 		let uri = urls.pages.MY_ENERGY + "?city=" + city + "&weather=" + weather + "&winp=" + winp + "&air_scope=" + air_scope + "";
 		return (
