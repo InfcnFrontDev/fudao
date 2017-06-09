1cf07608a39d214286718c9cc2ea2554b8b76655
zhangyan <459685710@qq.com>
2017-06-09 18:09:55 +0800

    顿号

--- app/views/disease/ExpectDetail.js
+++ app/views/disease/ExpectDetail.js
@@ -60,7 +60,7 @@ export default class ExpectDetail extends PureComponent {
 					{/*<Button transparent style={styles.btnStyle} onPress={()=> questionStore.jlModalShow=true}>*/}
 						{/*<Image source={require('../../assets/disease/jingluo.png')} style={styles.image}/>*/}
 					{/*</Button>*/}
-                    {allExpectListStore.selectedItem.type == '精神' ? <View/> : <Button transparent style={styles.btnStyle} onPress={() => Actions.body({pageKey:'disease',title:selectedItem.name})}>
+                    {allExpectListStore.selectedItem.part == '精神' ? <View/> : <Button transparent style={styles.btnStyle} onPress={() => Actions.body({pageKey:'disease',title:selectedItem.name})}>
                         <Image source={require('../../assets/disease/jingluo.png')} style={styles.image}/>
                     </Button>}
 				</Content>
